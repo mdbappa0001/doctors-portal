89f80e49e57714ef501fe9dfb9d69b8a77c77d92
mdbappa0001 <abutanif366@gmail.com>
2022-05-17 15:02:20 +0600

    backend connedted

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './Pages/About/About';
+import Appointment from './Pages/Appointment/Appointment';
 import Home from './Pages/Home/Home';
 import Login from './Pages/Login/Login';
+import RequireAuth from './Pages/Login/RequireAuth';
+import SignUp from './Pages/Login/SignUp';
 import Navbar from './Pages/Shared/Navbar';
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import DashBoard from './Pages/DashBoard/DashBoard';
+import MyAppointment from './Pages/DashBoard/MyAppointment';
+import MyReview from './Pages/DashBoard/MyReview';
+import MyHistory from './Pages/DashBoard/MyHistory';
 
 function App() {
   return (
-    <div>
+    <div className='max-w-7xl mx-auto px-12'>
     <Navbar></Navbar>
     <Routes>
       <Route path='/' element={<Home></Home>}></Route>
       <Route path='/about' element={<About></About>}></Route>
+      <Route  path='/appointment' element={
+      <RequireAuth>
+        <Appointment></Appointment>
+        </RequireAuth>
+      }></Route>
+
+      <Route  path='/dashboard' element={<RequireAuth><DashBoard></DashBoard></RequireAuth>}>
+        <Route index element={<MyAppointment></MyAppointment>}></Route>
+        <Route path='review' element={<MyReview></MyReview>}></Route>
+        <Route path='history' element={<MyHistory></MyHistory>}></Route>
+      </Route>
+
       <Route path='/login' element={<Login></Login>}></Route>
+      <Route path='/signup' element={<SignUp></SignUp>}></Route>
     </Routes>
+    <ToastContainer />
     </div>
   );
 }
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import Banner from './Banner';
+import Contact from './Contact';
+import Footer from '../Shared/Footer';
 import Info from './Info';
+import MakeAppointment from './MakeAppointment';
+import ServiceDetail from './ServiceDetail';
+import Services from './Services';
+import Testimonials from './Testimonials';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Info></Info>
+            <Services></Services>
+            <ServiceDetail></ServiceDetail>
+            <MakeAppointment></MakeAppointment>
+            <Testimonials></Testimonials>
+            <Contact></Contact>
+            <Footer></Footer>
         </div>
     );
 };
